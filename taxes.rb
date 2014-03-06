69c1169bf177d5827e5f302c41e38d4734cd0681
RohanPandula <rpandula@uh.edu>
2014-03-06 14:51:49 -0500

    first commit

---
+++ taxes.rb
@@ -0,0 +1,8 @@
+class Taxes
+  def initialize(first_name,last_name,annual_income,tax_paid,tax_rate)
+    @first_name = first_name
+    @last_name = last_name
+    @annual_income = annual_income
+    @tax_paid = tax_paid
+    @tax_rate = tax_rate
+  end
