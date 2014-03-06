98b30b818ed9c67575502bb0a08818909d3fe717
RohanPandula <rpandula@uh.edu>
2014-03-06 15:10:15 -0500

    read from csv

--- taxes.rb
+++ taxes.rb
@@ -1,3 +1,6 @@
+require 'csv'
+
+
 class Taxes
   def initialize(first_name,last_name,annual_income,tax_paid,tax_rate)
     @first_name = first_name
@@ -5,4 +8,23 @@ class Taxes
     @annual_income = annual_income
     @tax_paid = tax_paid
     @tax_rate = tax_rate
+    @people = []
   end
+  #attr_reader :first_name :last_name :annual_income :tax_rate :tax_paid
+
+  def csv_reader
+    CSV.foreach("tdd.csv", headers: true) do |row|
+      @people << {first_name:row[0], last_name:row[1], annual_income:row[2], tax_rate:row[3], tax_paid:row[4]}
+    end
+  end
+
+  def taxman
+  end
+
+ def plus_or_minus
+ end
+
+end
+
+
+puts people
