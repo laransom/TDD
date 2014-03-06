3904080e78767a5a93bc0ffa509131d9db972954
RohanPandula <rpandula@uh.edu>
2014-03-06 15:51:48 -0500

    fucking with the code

--- taxes.rb
+++ taxes.rb
@@ -1,6 +1,6 @@
 require 'csv'
-
-
+#
+#
 class Taxes
   def initialize(first_name,last_name,annual_income,tax_paid,tax_rate)
     @first_name = first_name
@@ -10,21 +10,29 @@ class Taxes
     @tax_rate = tax_rate
     @people = []
   end
-  #attr_reader :first_name :last_name :annual_income :tax_rate :tax_paid
-
-  def csv_reader
-    CSV.foreach("tdd.csv", headers: true) do |row|
-      @people << {first_name:row[0], last_name:row[1], annual_income:row[2], tax_rate:row[3], tax_paid:row[4]}
-    end
-  end
-
-  def taxman
-  end
-
- def plus_or_minus
- end
-
+  attr_accessor :first_name :last_name :annual_income :tax_rate :tax_paid
 end
-
-
+#
+#  def csv_reader
+#    CSV.foreach("tdd.csv", headers: true) do |row|
+#      @people << self.new ( {first_name:row[0], last_name:row[1], annual_income:row[2], tax_rate:row[3], tax_paid:row[4]} )
+#    end
+#  end
+#
+#  def instantiate
+#
+#  end
+#
+#  def taxman
+#  end
+#
+# def plus_or_minus
+# end
+#
+#end
+#
+people = []
+CSV.foreach("tdd.csv", headers: true) do |row|
+      people << Taxes.new({first_name:row[0], last_name:row[1], annual_income:row[2], tax_rate:row[3], tax_paid:row[4]})
+    end
 puts people
